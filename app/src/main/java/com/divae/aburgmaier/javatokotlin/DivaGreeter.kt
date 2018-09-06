31e9bfe2fa05b18552de883cd805718fdf392f2a
Aljoscha Burgmaier <aljoscha.burgmaier@diva-e.com>
2018-09-06 14:33:13 +0200

    Configured Kotlin
    - Added a Greeter Service.
    - Configured Kotlin via Android Studio

---
+++ app/src/main/java/com/divae/aburgmaier/javatokotlin/DivaGreeter.kt
@@ -0,0 +1,8 @@
+package com.divae.aburgmaier.javatokotlin
+
+class DivaGreeter(val name: String) {
+
+    fun greet(): String {
+        return "Grüße gehen raus an $name von allen Diven."
+    }
+}
